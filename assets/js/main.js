ad4c7ed5a4d4e96234b9731a63772f25e5c89c94
Andrew Mason <andrewmasonmedia@gmail.com>
2018-11-27 17:26:20 -0500

    Remake of basic site

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,32 @@
-//add plugin init here
-//$(document).ready(function() {
-//        $('.dropdown-toggle').dropdown();
-//    });
+//$('#main').smoothState({ blacklist: '.no-smoothState' });
 
 
 $(function(){
   'use strict';
-  var $page = $('#main'),
-      options = {
-        debug: true,
-        prefetch: true,
-        cacheLength: 10,
-        onStart: {
-//          duration: 450, // Duration of our animation
-          duration: 1000, // Duration of our animation
-          render: function ($container) {
-            // Add your CSS animation reversing class
-            $container.addClass('is-exiting');
-            // Restart your animation
-            smoothState.restartCSSAnimations();
-          }
-        },
-        onReady: {
-          //duration: 450,
-          duration: 1000,
-          render: function ($container, $newContent) {
-            // Remove your CSS animation reversing class
-            $container.removeClass('is-exiting');
-            // Inject the new content
-            $container.html($newContent);
-          }
-        },
-          //add onAfter --> https://stackoverflow.com/questions/35620499/reinitializing-page-scripts-after-smoothstate-js-page-change
-          onAfter: function() {
-//                $('.dropdown-toggle').dropdown();
-                $('.carousel').carousel({
-                  interval: 2000
-                })
-            }
-      },
-      smoothState = $page.smoothState(options).data('smoothState');
-});
+  var options = {
+    prefetch: true,
+    cacheLength: 2,
+    onStart: {
+      duration: 1000, // Duration of our animation
+      render: function ($container) {
+        // Add your CSS animation reversing class
+        $container.addClass('is-exiting');
 
-$('.carousel').carousel({
-  interval: 2000
-})
+        // Restart your animation
+        smoothState.restartCSSAnimations();
+      }
+    },
+    onReady: {
+      duration: 0,
+      render: function ($container, $newContent) {
+        // Remove your CSS animation reversing class
+        $container.removeClass('is-exiting');
+
+        // Inject the new content
+        $container.html($newContent);
+
+      }
+    }
+  },
+  smoothState = $('#main').smoothState(options).data('smoothState');
+});
